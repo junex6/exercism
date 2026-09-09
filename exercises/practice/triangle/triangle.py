09faf04644d64b1e8c056d4ac159209da5f410b9
junex6 <june@alumni.nd.edu>
2026-09-09 14:55:28 +0800

    Add triangle classification functions
    Implemented functions to classify triangle types based on side lengths.

--- exercises/practice/triangle/triangle.py
+++ exercises/practice/triangle/triangle.py
@@ -1,10 +1,15 @@
 def equilateral(sides):
-    pass
+    return triangle(sides) and len(set(sides)) == 1
 
 
 def isosceles(sides):
-    pass
+    return triangle(sides) and len(set(sides)) <= 2
 
 
 def scalene(sides):
-    pass
+    return triangle(sides) and len(set(sides)) == 3
+
+
+def triangle(sides):
+    a, b, c = sorted(sides)
+    return a > 0 and a + b >= c
