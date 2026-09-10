50f529a841d237228c2d8a9340bee6639187129d
junex6 <june@alumni.nd.edu>
2026-09-10 14:09:22 +0800

    Implement raindrops conversion logic

--- exercises/practice/raindrops/raindrops.py
+++ exercises/practice/raindrops/raindrops.py
@@ -1,2 +1,9 @@
 def convert(number):
-    pass
+    result = str()
+    if number % 3 == 0:
+        result += 'Pling'
+    if number % 5 == 0:
+        result += 'Plang'
+    if number % 7 == 0:
+        result += 'Plong' 
+    return result or str(number)
