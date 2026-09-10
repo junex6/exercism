7c4af6e40a6dc84e387e57e6537210db33b30a35
junex6 <june@alumni.nd.edu>
2026-09-10 15:01:14 +0800

    Add classification function for perfect numbers
    Implement classification of integers as deficient, perfect, or abundant based on their aliquot sum.

--- exercises/practice/perfect-numbers/perfect_numbers.py
+++ exercises/practice/perfect-numbers/perfect_numbers.py
@@ -4,4 +4,22 @@ def classify(number):
     :param number: int a positive integer
     :return: str the classification of the input integer
     """
-    pass
+    if number < 1:
+        raise ValueError('Classification is only possible for positive integers.')
+
+    if number == 1:
+        return 'deficient'
+
+    aliquot_sum = 1
+    for num in range(2, int(number**0.5)+1):
+        if number % num == 0:
+            aliquot_sum += num
+            partner = number // num
+            if partner != num:
+                aliquot_sum += partner
+            
+    if aliquot_sum == number:
+        return 'perfect'
+    if aliquot_sum > number:
+        return 'abundant'
+    return 'deficient'
