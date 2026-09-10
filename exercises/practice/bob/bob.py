eb65519f36bb8d4618233a32391ead2a2c178c84
junex6 <june@alumni.nd.edu>
2026-09-10 13:37:21 +0800

    Implement response function for Bob's replies

--- exercises/practice/bob/bob.py
+++ exercises/practice/bob/bob.py
@@ -1,2 +1,16 @@
 def response(hey_bob):
-    pass
+    text = hey_bob.strip()
+
+    isyelling = text.isupper()
+    isquestion = text.endswith('?')
+    
+    if not text:
+        return 'Fine. Be that way!'
+    if isyelling and isquestion:
+        return 'Calm down, I know what I\'m doing!'
+    if isquestion:
+        return 'Sure.'
+    if isyelling:
+        return 'Whoa, chill out!'
+    
+    return 'Whatever.'
