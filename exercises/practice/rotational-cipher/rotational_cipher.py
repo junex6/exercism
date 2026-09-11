8158e5ce902e3af3c330a8c16bd848b882ce9c23
junex6 <june@alumni.nd.edu>
2026-09-11 15:13:02 +0800

    Add rotational cipher implementation
    Implement rotational cipher logic to rotate letters based on the given key.

--- exercises/practice/rotational-cipher/rotational_cipher.py
+++ exercises/practice/rotational-cipher/rotational_cipher.py
@@ -1,2 +1,10 @@
 def rotate(text, key):
-    pass
+    lower = "abcdefghijklmnopqrstuvwxyz"
+    upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    
+    shifted_lower = lower[key:] + lower[:key]
+    shifted_upper = upper[key:] + upper[:key]
+    
+    trans_table = str.maketrans(lower + upper, shifted_lower + shifted_upper)
+    
+    return text.translate(trans_table)
