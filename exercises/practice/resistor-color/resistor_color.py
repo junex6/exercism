bae28b016bef0016d1524d6d147d1ad9572f1d5a
junex6 <june@alumni.nd.edu>
2026-09-15 12:41:20 +0800

    Implement color_code and colors functions

--- exercises/practice/resistor-color/resistor_color.py
+++ exercises/practice/resistor-color/resistor_color.py
@@ -1,6 +1,18 @@
 def color_code(color):
-    pass
+    color_list = colors()
+    return color_list.index(color)
 
 
 def colors():
-    pass
+    return [
+        "black", 
+        "brown", 
+        "red", 
+        "orange", 
+        "yellow", 
+        "green", 
+        "blue", 
+        "violet", 
+        "grey", 
+        "white"
+    ]
