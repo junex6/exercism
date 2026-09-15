a866231a68ca9346f7f04ae0a01bfb7bf42a1737
junex6 <june@alumni.nd.edu>
2026-09-15 14:47:57 +0800

    Implement find_anagrams function

--- exercises/practice/anagram/anagram.py
+++ exercises/practice/anagram/anagram.py
@@ -1,2 +1,7 @@
 def find_anagrams(word, candidates):
-    pass
+    word_lower = word.lower()
+    word_sorted = sorted(word_lower)
+    
+    return [c for c in candidates 
+            if word_lower != (c_lower := c.lower())
+            and word_sorted == sorted(c_lower)]
