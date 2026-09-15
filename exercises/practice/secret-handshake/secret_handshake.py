901905b14c62dcdf4b97cf48b3dfc1a235523952
junex6 <june@alumni.nd.edu>
2026-09-15 14:20:47 +0800

    Implement secret handshake logic in commands function

--- exercises/practice/secret-handshake/secret_handshake.py
+++ exercises/practice/secret-handshake/secret_handshake.py
@@ -1,2 +1,11 @@
+ACTIONS = [
+    "wink",
+    "double blink",
+    "close your eyes",
+    "jump",
+]
+
 def commands(binary_str):
-    pass
+    bits = binary_str.zfill(5)
+    actions = [ACTIONS[i] for i, bit in enumerate(reversed(bits[-4:])) if bit == "1"]
+    return actions[::-1] if bits[0] == "1" else actions
