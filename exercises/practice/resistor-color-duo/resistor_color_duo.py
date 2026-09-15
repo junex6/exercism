a50370901765cd400906add371f2b900aff57097
junex6 <june@alumni.nd.edu>
2026-09-15 12:53:18 +0800

    Implement resistor color value calculation

--- exercises/practice/resistor-color-duo/resistor_color_duo.py
+++ exercises/practice/resistor-color-duo/resistor_color_duo.py
@@ -1,2 +1,15 @@
+COLOR_CODE = [
+    "black", 
+    "brown", 
+    "red", 
+    "orange", 
+    "yellow", 
+    "green", 
+    "blue", 
+    "violet", 
+    "grey", 
+    "white"
+]
+
 def value(colors):
-    pass
+    return COLOR_CODE.index(colors[0]) * 10 + COLOR_CODE.index(colors[1])
