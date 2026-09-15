88d933d0315f5f96d277c8d4fa4b0890583f530f
junex6 <june@alumni.nd.edu>
2026-09-15 13:17:58 +0800

    Add resistor color code calculation logic
    Implement resistor color code calculation in label function.

--- exercises/practice/resistor-color-trio/resistor_color_trio.py
+++ exercises/practice/resistor-color-trio/resistor_color_trio.py
@@ -1,2 +1,27 @@
+COLOR_CODE = [
+    "black", 
+    "brown", 
+    "red", 
+    "orange", 
+    "yellow", 
+    "green", 
+    "blue", 
+    "violet", 
+    "grey", 
+    "white"
+]
+
+
 def label(colors):
-    pass
+    base = COLOR_CODE.index(colors[0]) * 10 + COLOR_CODE.index(colors[1])
+    exponents = COLOR_CODE.index(colors[2])
+    number = base * (10 ** exponents)
+
+    prefixes = ["", "kilo", "mega", "giga"]
+    prefix_idx = 0
+
+    while number > 0 and number % 1000 == 0 and prefix_idx < len(prefixes) - 1: 
+        number //= 1000
+        prefix_idx += 1
+    
+    return f"{number} {prefixes[prefix_idx]}ohms"
