4a11f7703f213f979a0b46862682c377cd8efc45
junex6 <june@alumni.nd.edu>
2026-09-17 16:48:46 +0800

    Add binary search implementation to find function
    Implement binary search algorithm in find function.

--- exercises/practice/binary-search/binary_search.py
+++ exercises/practice/binary-search/binary_search.py
@@ -1,2 +1,18 @@
 def find(search_list, value):
-    pass
+    if not search_list:
+        raise ValueError("value not in array")
+    
+    low = 0
+    high = len(search_list) - 1
+    
+    while low <= high:
+        mid = (high + low) // 2
+        
+        if search_list[mid] == value:
+            return mid
+        elif search_list[mid] < value:
+            low = mid + 1
+        else:
+            high = mid - 1
+    
+    raise ValueError("value not in array")
