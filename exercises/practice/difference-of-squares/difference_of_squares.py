adae622d18c3fa52291c796a1b3a34ef1340f3a5
junex6 <june@alumni.nd.edu>
2026-09-20 14:02:27 +0800

    Implement functions for difference of squares

--- exercises/practice/difference-of-squares/difference_of_squares.py
+++ exercises/practice/difference-of-squares/difference_of_squares.py
@@ -1,10 +1,12 @@
 def square_of_sum(number):
-    pass
+    """Calculates the square of the sum of the first n natural numbers: sum(range(number+1)) ** 2"""
+    return (number * (number + 1) / 2) ** 2
 
 
 def sum_of_squares(number):
-    pass
+    """Calculates the sum of the squares of the first n natural numbers: sum([x ** 2 for x in range(number+1)])"""
+    return number * (number + 1) * (2 * number + 1) / 6
 
 
 def difference_of_squares(number):
-    pass
+    return square_of_sum(number) - sum_of_squares(number)
