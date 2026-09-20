47cfd8c3294a13c8fcf661bbd07decbd51db6cfd
junex6 <june@alumni.nd.edu>
2026-09-20 13:04:21 +0800

    Implement line_up function to format customer message

--- exercises/practice/line-up/line_up.py
+++ exercises/practice/line-up/line_up.py
@@ -1,2 +1,6 @@
 def line_up(name, number):
-    pass
+    if 11 <= number % 100 <= 13:
+        suffix = "th"
+    else:
+        suffix = {1: "st", 2: "nd", 3:"rd"}.get(number % 10, "th")
+    return f"{name}, you are the {number}{suffix} customer we serve today. Thank you!"
