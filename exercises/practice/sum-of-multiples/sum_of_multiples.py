856b1e9f9225cbd8ea3cf0266fa2eb10015e49a0
junex6 <june@alumni.nd.edu>
2026-09-21 16:57:29 +0800

    Implement sum_of_multiples function

--- exercises/practice/sum-of-multiples/sum_of_multiples.py
+++ exercises/practice/sum-of-multiples/sum_of_multiples.py
@@ -1,2 +1,7 @@
 def sum_of_multiples(limit, multiples):
-    pass
+    energy_points = {
+        value
+        for item in multiples if item > 0
+        for value in range(item, limit, item)
+    }
+    return sum(energy_points)
