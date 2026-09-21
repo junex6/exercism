07f0a8c2a9a0cf22ddfca1092424620bdfe03d18
junex6 <june@alumni.nd.edu>
2026-09-21 15:50:47 +0800

    Add SpaceAge class with planet age calculations
    Implement SpaceAge class to calculate age on different planets.

--- exercises/practice/space-age/space_age.py
+++ exercises/practice/space-age/space_age.py
@@ -1,3 +1,45 @@
 class SpaceAge:
+    EARTH_YEAR_SECONDS = 31557600
+    
+    PLANET_ORBITAL_PERIODS = {
+        "mercury": 0.2408467,
+        "venus": 0.61519726,
+        "earth": 1.0,
+        "mars": 1.8808158,
+        "jupiter": 11.862615,
+        "saturn": 29.447498,
+        "uranus": 84.016846,
+        "neptune": 164.79132,
+    }
+    
     def __init__(self, seconds):
-        pass
+        self.seconds = seconds
+
+    def _calculate_age(self, planet):
+        earth_years = self.seconds / self.EARTH_YEAR_SECONDS
+        planet_years = earth_years / self.PLANET_ORBITAL_PERIODS[planet]
+        return round(planet_years, 2)
+    
+    def on_earth(self):
+        return self._calculate_age("earth")
+
+    def on_mercury(self):
+        return self._calculate_age("mercury")
+    
+    def on_venus(self):
+        return self._calculate_age("venus")
+
+    def on_mars(self):
+        return self._calculate_age("mars")
+
+    def on_jupiter(self):
+        return self._calculate_age("jupiter")
+
+    def on_saturn(self):
+        return self._calculate_age("saturn")
+
+    def on_uranus(self):
+        return self._calculate_age("uranus")
+
+    def on_neptune(self):
+        return self._calculate_age("neptune")
