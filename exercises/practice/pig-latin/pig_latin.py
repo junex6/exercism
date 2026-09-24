9922935b1401d47eddf6627e23db5f610c561985
junex6 <june@alumni.nd.edu>
2026-09-24 13:43:57 +0800

    Add Pig Latin translation function
    Implement Pig Latin translation logic using regex patterns.

--- exercises/practice/pig-latin/pig_latin.py
+++ exercises/practice/pig-latin/pig_latin.py
@@ -1,2 +1,20 @@
+import re
+
+PATTERN_VOWEL = re.compile(r"^(a|e|i|o|u|xr|yt)")
+PATTERN_CONSONANT = re.compile(r"^([bcdfghjklmnpqrstvwxyz]+)(.*)$")
+PATTERN_QU = re.compile(r"^([bcdfghjklmnpqrstvwxyz]*qu)(.*)$")
+PATTERN_Y = re.compile(r"^([bcdfghjklmnpqrstvwxyz]+)(y.*)$")
+
 def translate(text):
-    pass
+    return " ".join(piglatin(word) for word in text.split())
+
+def piglatin(word):
+    if PATTERN_VOWEL.match(word):
+        return f"{word}ay"
+    if match := PATTERN_QU.match(word):
+        return f"{match.group(2)}{match.group(1)}ay"
+    if match := PATTERN_Y.match(word):
+        return f"{match.group(2)}{match.group(1)}ay"
+    if match := PATTERN_CONSONANT.match(word):
+        return f"{match.group(2)}{match.group(1)}ay"
+    return word
