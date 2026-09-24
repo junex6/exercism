01e1bf78fd187ec5d06698cec55d9b2363e22a90
junex6 <june@alumni.nd.edu>
2026-09-24 15:04:06 +0800

    Add is_paired function to check bracket pairing
    Implement bracket matching logic using a stack.

--- exercises/practice/matching-brackets/matching_brackets.py
+++ exercises/practice/matching-brackets/matching_brackets.py
@@ -1,2 +1,12 @@
 def is_paired(input_string):
-    pass
+    mapping = {"]":"[", "}":"{", ")":"("}
+    stack = []
+    
+    for char in input_string:
+        if char in mapping.values():
+            stack.append(char)
+        if char in mapping:
+            if not stack or stack.pop() != mapping[char]:
+                return False
+
+    return len(stack) == 0
