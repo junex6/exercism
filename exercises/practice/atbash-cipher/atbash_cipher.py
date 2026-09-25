874d6355af5dd86491ac0e70b0fee1b29ecfb00d
junex6 <june@alumni.nd.edu>
2026-09-25 17:07:25 +0800

    Add encode and decode functions for Atbash cipher
    Implement Atbash cipher encoding and decoding functions.

--- exercises/practice/atbash-cipher/atbash_cipher.py
+++ exercises/practice/atbash-cipher/atbash_cipher.py
@@ -1,6 +1,14 @@
-def encode(plain_text):
-    pass
+PLAIN = "abcdefghijklmnopqrstuvwxyz"
+CIPHER = "zyxwvutsrqponmlkjihgfedcba"
+TRANS_MAP = str.maketrans(PLAIN, CIPHER)
 
+
+def encode(plain_text):
+    clean_text = "".join(char for char in plain_text.lower() if char.isalnum())
+    translated = clean_text.translate(TRANS_MAP)
+    return " ".join(translated[i:i + 5] for i in range(0, len(translated), 5))
+ 
 
 def decode(ciphered_text):
-    pass
+    clean_text = "".join(ciphered_text.lower().split())
+    return clean_text.translate(TRANS_MAP)
