767431bd0fe46598aff9a7d8e02545b8becb0b0d
junex6 <june@alumni.nd.edu>
2026-09-25 22:32:16 +0800

    Add function to generate diamond pattern
    Implement diamond pattern generation for given letter.

--- exercises/practice/diamond/diamond.py
+++ exercises/practice/diamond/diamond.py
@@ -1,2 +1,14 @@
 def rows(letter):
-    pass
+    alphabet = [chr(i) for i in range(ord("A"), ord(letter) + 1)]
+    width = len(alphabet) * 2 - 1
+    top_half = []
+    
+    for i, char in enumerate(alphabet):
+        if char == "A":
+            row = "A".center(width)
+        else:
+            inner_spaces = " " * (2 * i - 1)
+            row = f"{char}{inner_spaces}{char}".center(width)
+        top_half.append(row)
+
+    return top_half + top_half[-2::-1]
